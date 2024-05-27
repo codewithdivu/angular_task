2a5b7aa8f92187c3614198a1705a036e2e88b249
= <=>
2024-05-27 17:57:53 +0530

    [fixed] : validation in form

--- src/app/Components/user-form/user-form.component.ts
+++ src/app/Components/user-form/user-form.component.ts
@@ -8,11 +8,13 @@ import { MatIconModule } from '@angular/material/icon';
 
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import Validation from '../../../utils/Validation';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
@@ -27,18 +29,47 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class UserFormComponent {
   signupForm: FormGroup;
 
+  validationMessages = {
+    firstName: {
+      required: 'First name is required.',
+    },
+    email: {
+      required: 'Email is required.',
+      email: 'Please enter a valid email address.',
+    },
+    username: {
+      required: 'Username is required.',
+    },
+    password: {
+      required: 'Password is required.',
+    },
+    confirmPassword: {
+      required: 'Confirm Password is required.',
+      passwordMismatch: 'Passwords do not match.',
+    },
+  };
+
   constructor(private formBuilder: FormBuilder) {
-    this.signupForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-      termsOfUser: [false, Validators.requiredTrue],
-    });
+    this.signupForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+        termsOfUser: [false, Validators.requiredTrue],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
+    );
   }
 
   onSubmit() {
-    console.log(this.signupForm.value);
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    } else {
+      this.signupForm.markAllAsTouched();
+    }
   }
 }
